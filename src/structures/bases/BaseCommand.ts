6461ff117ac963a2d949ec9e02c58cae924562e4
João Victor <joaovictor-san-co@hotmail.com>
2020-12-23 10:17:10 -0300

    feat(structures/bases/BaseCommand.ts): Finished the base for creating new commands

--- src/structures/bases/BaseCommand.ts
+++ src/structures/bases/BaseCommand.ts
@@ -1,5 +1,22 @@
-import { Collection, Message } from "discord.js";
-import { NewClient } from "../NewClient";
+import { Collection, Message } from "discord.js"
+import { NewClient } from "../NewClient"
+import { defaultCommandCooldownTime, defaultCommandCooldownUsageLimit } from '../../../config.json'
+
+defaultCommandCooldownTime as number
+defaultCommandCooldownUsageLimit as number
+
+interface ICommandConfigReleasesNotesValuesParams {
+  version?: string
+  name: string
+  description: string
+  createdAt?: Date
+  createdTimestamp: number
+}
+
+interface ICommandConfigReleasesNotesValues extends ICommandConfigReleasesNotesValuesParams {
+  version: string
+  createdAt: Date
+}
 
 interface ICommandConfigParams {
   name: string
@@ -7,56 +24,81 @@ interface ICommandConfigParams {
   description?: string
   categories: string[] | string
   cooldown?: {
-    time: number
+    time?: number
     usageLimit?: number
   } | number
-  active?: boolean
+  isActive?: boolean
   reasonInactivity?: string
   createdAt?: Date
   createdTimestamp: number
   lastUpdateAt?: Date
   lastUpdateTimestamp: number
   version: string
-  releasesNotes: Collection<string, {
-    version: string
-    name: string
-    description: string
-    createdAt?: Date
-    createdTimestamp: number
-  }>
+  releasesNotes?: Collection<string, ICommandConfigReleasesNotesValuesParams>
 }
 
 interface ICommandConfig extends ICommandConfigParams {
+  aliases: string[]
+  categories: string[]
   cooldown: {
     time: number
     usageLimit: number
   }
-  active: boolean
+  isActive: boolean
   createdAt: Date
   lastUpdateAt: Date
-  releasesNotes: Collection<string, {
-    version: string
-    name: string
-    description: string
-    createdAt: Date
-    createdTimestamp: number
-  }>
+  releasesNotes: Collection<string, ICommandConfigReleasesNotesValues>
 }
 
-
-
-interface IConstructorParams {
-  config: ICommandConfig
-  run: (params: {
-    client?: NewClient
-    message?: Message
-    args?: string[]
-  }) => void
+interface ICommandParams {
+  config: ICommandConfigParams, 
+  run: (params: { client?: NewClient, message?: Message, args?: string[]}) => void
 }
 
 class BaseCommand {
   config: ICommandConfig
-  constructor({ config, run }: { config: ICommandConfigParams, run(params: { client?: NewClient, message?: Message, args?: string[]}): void }) {
-    this.config.releasesNotes.forEach(x => x.)
+  run: (params: { client?: NewClient, message?: Message, args?: string[]}) => void
+
+  constructor({ config, run }: ICommandParams) {
+    if (typeof config.aliases === 'string') config.aliases = [config.aliases]
+    if (typeof config.categories === 'string') config.categories = [config.categories]
+    if (!config.cooldown) config.cooldown = { time: defaultCommandCooldownTime, usageLimit: defaultCommandCooldownUsageLimit }
+    if (typeof config.cooldown === 'number') config.cooldown = { time: config.cooldown, usageLimit: defaultCommandCooldownUsageLimit }
+
+    const releasesNotesEntries = config.releasesNotes?.map((release, version) => {
+      const entry: [string, ICommandConfigReleasesNotesValues] = [
+        version,
+        {
+          version: release.version || version,
+          name: release.name,
+          description: release.description,
+          createdAt: release.createdAt ?? new Date(release.createdTimestamp),
+          createdTimestamp: release.createdTimestamp
+        }
+      ]
+
+      return entry
+    })
+
+    this.config = {
+      name: config.name,
+      aliases: config.aliases ?? [],
+      description: config.description,
+      categories: config.categories,
+      cooldown: {
+        time: config.cooldown.time ?? defaultCommandCooldownTime,
+        usageLimit: config.cooldown.usageLimit ?? 1
+      },
+      isActive: config.isActive ?? true,
+      reasonInactivity: config.reasonInactivity,
+      createdAt: config.createdAt ?? new Date(config.createdTimestamp),
+      createdTimestamp: config.createdTimestamp,
+      lastUpdateAt: config.lastUpdateAt ?? new Date(config.lastUpdateTimestamp),
+      lastUpdateTimestamp: config.lastUpdateTimestamp,
+      version: config.version,
+      releasesNotes: new Collection(releasesNotesEntries)
+    }
+
+    this.run = run
   }
 }
